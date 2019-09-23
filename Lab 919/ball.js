b5129fde3645c533e787e6280a80d428ac42cf67
dylan-p <31372610+dylan-p@users.noreply.github.com>
2019-09-23 10:30:42 -0700

    123

--- Lab 919/ball.js
+++ Lab 919/ball.js
@@ -1,25 +1,30 @@
-function ballClass(x, y, vx, vy, ax, ay, radius){
+function ballClass(x, y, vx, vy, ax, ay, radius, s1, s2, s3, c1, c2, c3){
   this.radius = radius;
   this.loc = new JSVector(x, y);
   this.vel = new JSVector(vx, vy);
   this.acc = new JSVector(ax, ay);
   this.other = new JSVector(cnv.width/2, cnv.height/2);
+  this.s1 = s1;
+  this.s2 = s2;
+  this.s3 = s3;
+  this.c1 = c1;
+  this.c2 = c2;
+  this.c3 = c3;
 }
 
 ballClass.prototype.render = function(){
-  ctx.strokeStyle = 'rgb(85, 107, 47)';
+  ctx.strokeStyle = "rgb(" + this.s1 + "," + this.s2 + "," + this.s3 + ")";
   ctx.lineWidth = '6';
-  ctx.fillStyle = 'rgb(255, 140, 0)';
+  ctx.fillStyle = "rgb(" + this.c1 + "," + this.c2 + "," + this.c3 + ")";
 
   ctx.beginPath();
   ctx.moveTo(this.loc.x, this.loc.y);
-  ctx.lineTo(this.loc.x, this.loc.y+this.radius);
-  ctx.lineTo(this.loc.x+this.radius, this.loc.y);
+  ctx.lineTo(this.loc.x, this.loc.y+(this.radius*Math.random()+3));
+  ctx.lineTo(this.loc.x+(this.radius*Math.random()+3), this.loc.y);
   ctx.closePath();
   //ctx.triange(this.loc.x, this.loc.y, this.loc.x+this.radius, this.loc.y+this.radius, this.loc.x-this.radius, this.loc.y+this.radius);
   ctx.fill();
   ctx.stroke();
-
 }
 
 ballClass.prototype.update = function(){
@@ -27,33 +32,32 @@ ballClass.prototype.update = function(){
   this.loc.add(this.vel);
 }
 
-ballClass.prototype.applyForce = function(v2){
-  /*this.acc.setMagnitude(0);
-  var force;
-  if(bool){
-    force = JSVector.addGetNew(loc, this.loc);
-  }
-  else{
-    force = JSVector.subGetNew(loc, this.loc)
-  }
-  force.normalize();
-  force.multiply(0.5);
-  this.acc.add(force);*/
+ballClass.prototype.quentin = function(v2){
   var d = this.loc.distance(v2.loc);
-  if(d<500){
+  if(d<175){
     var attractionForce = JSVector.subGetNew(v2.loc, this.loc);
-    //finish next class
+    attractionForce.normalize();
+    attractionForce.multiply(0.05);
+    this.acc.add(attractionForce);
   }
 }
 
-
+ballClass.prototype.eric = function(v2){
+  var d = this.loc.distance(v2.loc);
+  if(d<175){
+    var attractionForce = JSVector.subGetNew(this.loc, v2.loc);
+    attractionForce.normalize();
+    attractionForce.multiply(0.05);
+    this.acc.add(attractionForce);
+  }
+}
 
 ballClass.prototype.checkEdges = function(){
   if(this.loc.x + this.radius > cnv.width || this.loc.x - this.radius < 0){
     this.vel.x = -this.vel.x;
   }
 
-  if(this.loc.y + this.radius > cnv.height || this.loc.x - this.radius < 0){
+  if(this.loc.y + this.radius > cnv.height || this.loc.y - this.radius < 0){
     this.vel.y = -this.vel.y;
   }
 }
--- Lab 919/jssecond.js
+++ Lab 919/jssecond.js
@@ -18,9 +18,9 @@ function init(){
   cnv.style.backgroundColor = 'rgba(0,44,55, 0.1575)';
   ctx = cnv.getContext('2d');
   for(let a = 0; a<numBal; a++){
-    ball[a] = new ballClass(Math.random()*window.innerWidth, Math.random()*window.innerHeight, Math.random()*3, -Math.random()*3, 0, 0.03, 30*Math.random())
+    ball[a] = new ballClass(Math.random()*window.innerWidth, Math.random()*window.innerHeight, Math.random()*3, -Math.random()*3, 0, 0.03, 50*Math.random(), 255*Math.random(), 255*Math.random(), 255*Math.random(), 255*Math.random(), 255*Math.random(), 255*Math.random())
 }
-  repulser = new ballClass(Math.random()*window.innerWidth, Math.random()*window.innerHeight, 0, 0, 0, 0, 30*Math.random())
+  repulser = new ballClass(350, 350, 0.1, 0.1, 0, 0, 30*Math.random()+15, 0, 0, 0, 255, 255, 255);
 /*for(let a= 0; a<numBal; a++){
   loc[a] = new JSVector(Math.random()*window.innerWidth, Math.random()*window.innerHeight);
   vel[a] = new JSVector(Math.random()*3, -Math.random()*3);
@@ -34,24 +34,8 @@ function animate(){
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   for(let a = 0; a<numBal; a++){
-  /*if(ball[a].loc.distance(repulser.loc) < 200){
-    ball[a].applyForce(true, repulser.loc);
-  }*/
+  ball[a].quentin(repulser);
   ball[a].run();
-  repulser.run();
 }
-/*
-  ctx.strokeStyle = 'rgb(85, 107, 47)';
-  ctx.lineWidth = '10';
-  ctx.fillStyle = 'rgb(255, 140, 0)';
-    for(let a = 0; a<numBal; a++){
-      ctx.beginPath();
-      ctx.arc(loc[a].x,loc[a].y, radius[a], Math.PI*2, 0, false);
-      ctx.fill();
-      ctx.stroke();
-      vel[a].add(accel);
-      loc[a].add(vel[a]);
-        if(loc[a].x > window.innerWidth || loc[a].x<0) vel[a].x = -vel[a].x;
-        if(loc[a].y > window.innerHeight || loc[a].y<0) vel[a].y = -vel[a].y;
-      }*/
+repulser.run();
 }
