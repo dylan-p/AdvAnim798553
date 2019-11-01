a4b7bc6a7bb612b6855ef7567aa2908dc706e2c8
dylan-p <31372610+dylan-p@users.noreply.github.com>
2019-11-01 10:21:37 -0700

    Merge branch 'master' of https://github.com/dylan-p/AdvAnim798553

--- Lab 1028 Snake/snake.js
+++ Lab 1028 Snake/snake.js
@@ -4,13 +4,7 @@ function snakeClass(x, y, vx, vy, ax, ay, radius, s1, s2, s3, c1, c2, c3, orbRad
   this.loc = new JSVector(x, y);
   this.vel = new JSVector(vx, vy);
   this.points = [];
-  this.points.push(new JSVector(x, y-10));
-  this.points.push(new JSVector(x, y-30));
-  this.points.push(new JSVector(x, y-50));
-  this.points.push(new JSVector(x, y-70));
   this.acc = new JSVector(0, 0);
-//   this.other = new JSVector(cnv.width/2, cnv.height/2);
-//   this.id = weer;
 //
 //   // color values
   this.s1 = s1;
@@ -21,7 +15,7 @@ function snakeClass(x, y, vx, vy, ax, ay, radius, s1, s2, s3, c1, c2, c3, orbRad
   this.c3 = c3;
   // color values
   this.orbiter = [];
-  this.weer = weer;
+  this.id = weer;
   // for(let a = 0; a<numOrb; a++){
   //     this.orbiter[a] = new Orbiter(9, 0, 0, orbRad, ((2*Math.PI)/(numOrb)*a), this.loc, this);
   // }
@@ -29,40 +23,30 @@ function snakeClass(x, y, vx, vy, ax, ay, radius, s1, s2, s3, c1, c2, c3, orbRad
 
 snakeClass.prototype.render = function(){
   ctx.strokeStyle = "rgb(" + this.s1 + 20 + "," + this.s2 + 20 + "," + this.s3 + 20 + ")";
-  ctx.lineWidth = '6';
+  ctx.lineWidth = '1';
   ctx.fillStyle = "rgb(" + this.c1 + 20 + "," + this.c2 + 20 + "," + this.c3 + 20 + ")";
-
-  ctx.save();
-  ctx.translate(this.loc.x, this.loc.y);
-  ctx.rotate(this.vel.getDirection() - (Math.PI/2));
-
+  //makes the head
   ctx.beginPath();
-  ctx.moveTo(0,0);
-  ctx.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI*2, true);
-  ctx.closePath();
-  for(let a = 0; a<this.points.length; a++){
-    ctx.arc(this.points[a].x*0.2, this.points[a].y*0.2, 5, 0, Math.PI*2, true);
-  }
-
-//  ctx.scale(((Math.random()*3)+3), ((Math.random()*3)+3));
-
+  ctx.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI*2, false);
   ctx.stroke();
   ctx.fill();
-
-  ctx.restore();
-
-  // for(let a = 0; a<numOrb; a++){
-  //   this.orbiter[a].render();
-  // }
+  //makes the tail
+  for(let a = 0; a<this.points.length; a++){
+    ctx.beginPath();
+    ctx.arc(this.points[a].x, this.points[a].y, this.radius/2, 0, Math.PI*2, false);
+    ctx.stroke();
+    ctx.fill();
+  }
 }
 
 snakeClass.prototype.update = function(){
   this.vel.add(this.acc);
   this.vel.limit(2);
   this.loc.add(this.vel);
-  for(let a = 0; a<this.points.length; a++){
-    this.points[a].x = this.loc.x;
-    this.points[a].y = this.loc.y;
+  var newPoint = new JSVector(this.loc.x, this.loc.y);
+  this.points.push(newPoint);
+  if(this.points.length>50){
+    this.points.splice(0, 1);
   }
 }
 
